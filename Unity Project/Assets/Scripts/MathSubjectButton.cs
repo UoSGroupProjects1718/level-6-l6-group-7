13c420ddbc360f7a111ffc7490d3361dd0f0332e
Ben Reynolds <breynolds1994@outlook.com>
2017-11-15 15:18:04 +0000

    Implement scene transition for subject selection

---
+++ Unity Project/Assets/Scripts/MathSubjectButton.cs
@@ -0,0 +1,7 @@
+﻿public class MathSubjectButton : SubjectButtonPress {
+    public override void OnPress()
+    {
+        InitiateTransitionAnimation();
+        StartCoroutine(SceneTransitioner.TransitionTo("Subject Selection", true, 1.5f, 1.5f));
+    }
+}
