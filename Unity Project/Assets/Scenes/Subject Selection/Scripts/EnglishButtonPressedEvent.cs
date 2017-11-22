105e2f3aa22c51c830dfcf4d3dc2f545f02de2da
Ben Reynolds <breynolds1994@outlook.com>
2017-11-22 14:43:58 +0000

    Refactor Subject Selection

---
+++ Unity Project/Assets/Scenes/Subject Selection/Scripts/EnglishButtonPressedEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class EnglishButtonPressedEvent : SubjectButtonPressedEvent
+{
+	[SerializeField] private GameObject _mathOverlay;
+	[SerializeField] private GameObject _scienceOverlay;
+	
+	private const string SubjectName = "English";
+	
+	protected override void PerformTransition()
+	{
+		GameManager gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+		gameManager.SelectedSubject = gameManager.English;
+		
+		_mathOverlay.GetComponent<Animator>().SetBool(SubjectName, true);
+		_scienceOverlay.GetComponent<Animator>().SetBool(SubjectName, true);
+	}
+}
---
+++ Unity Project/Assets/Scenes/Subject Selection/Scripts/MathButtonPressedEvent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MathButtonPressedEvent : SubjectButtonPressedEvent
+{
+    [SerializeField] private GameObject _englishOverlay;
+    [SerializeField] private GameObject _scienceOverlay;
+    
+    protected override void PerformTransition()
+    {
+        GameManager gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        gameManager.SelectedSubject = gameManager.Math;
+        
+        _englishOverlay.GetComponent<Animator>().SetBool(SubjectName, true);
+        _scienceOverlay.GetComponent<Animator>().SetBool(SubjectName, true);
+    }
+}
