d55c7bf4c6445cbdf00a91302ca19ac4a65254da
Ben Reynolds <breynolds1994@outlook.com>
2018-01-17 13:41:04 +0000

    Add exit and sticker board buttons to the subject selectio screen and fix related bugs

---
+++ Unity Project/Assets/Scenes/Difficulty Selection/Scripts/DifficultyBackButtonPressedEvent.cs
@@ -0,0 +1,13 @@
+﻿using General.Scripts;
+
+namespace Scenes.Difficulty_Selection.Scripts
+{
+    public class DifficultyBackButtonPressedEvent : ButtonPressedEvent {
+    
+        protected override void ButtonAction()
+        {
+            GameManager.Instance.ActiveSubject = null;
+        }
+    
+    }
+}
