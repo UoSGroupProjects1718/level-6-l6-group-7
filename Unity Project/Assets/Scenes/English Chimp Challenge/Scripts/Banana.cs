742b977883c83d36b0f7417216dc61950c3c9f67
Ben Reynolds <breynolds1994@outlook.com>
2018-01-18 15:42:50 +0000

    Add bucket dragging animation and create draggable banana object

---
+++ Unity Project/Assets/Scenes/English Chimp Challenge/Scripts/Banana.cs
@@ -0,0 +1,38 @@
+﻿using General.Scripts;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Scenes.English_Chimp_Challenge.Scripts
+{
+    public class Banana : DraggableObject
+    {
+        private RectTransform _rectTransform;
+
+        private void Start()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+            Debug.Log("BANANA");
+        }
+    
+        public override void OnBeginDrag(PointerEventData eventData)
+        {
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            Debug.Log("Begin Drag");
+        }
+
+        public override void OnDrag(PointerEventData eventData)
+        {
+            Debug.Log("On Drag");
+            _rectTransform.position = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            _rectTransform.localPosition = new Vector3(_rectTransform.localPosition.x,
+                _rectTransform.localPosition.y,
+                0);
+        }
+
+        public override void OnEndDrag(PointerEventData eventData)
+        {
+            Debug.Log("End Drag");
+        }
+    
+    }
+}
