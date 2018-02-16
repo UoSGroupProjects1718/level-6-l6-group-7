1e63bf66e14caf61a7c885286387a3e38952e71a
Ben Reynolds <breynolds1994@outlook.com>
2018-02-16 12:49:18 +0000

    Minor bug fixes/UI changes

---
+++ Unity Project/Assets/Scenes/Sticker Board/Scripts/StickerBoardBackButtonPressedEvent.cs
@@ -0,0 +1,16 @@
+﻿using General.Scripts;
+using UnityEngine;
+
+namespace Scenes.Sticker_Board.Scripts
+{
+	public class StickerBoardBackButtonPressedEvent : ButtonPressedEvent {
+
+		protected override void ButtonAction()
+		{
+			var gameManager = GameManager.Instance;
+			var sceneTransitioner = GameObject.Find("Scene Transitioner").GetComponent<SceneTransitioner>();
+			sceneTransitioner.TransitionToScene(gameManager.ActiveSubject != null ? "Difficulty Selection" : "Subject Selection");
+		}
+	
+	}
+}
