aafd357bde8edb839c2a21240c09c883c3e6eaaf
Ben Reynolds <breynolds1994@outlook.com>
2018-04-24 13:53:00 +0100

    Create Science Challenge scene and ScriptableObject for ScienceChallengeQuestions

---
+++ Unity Project/Assets/Scenes/Science Challenge/Scripts/PhotoFrame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scenes.Science_Challenge.Scripts
+{
+    public class PhotoFrame : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+
+        public void SetPhoto(Sprite sprite)
+        {
+            _button.image.sprite = sprite;
+        }
+    }
+}
---
+++ Unity Project/Assets/Scenes/Science Challenge/Scripts/ScienceQuestion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scenes.Science_Challenge.Scripts
+{
+    [CreateAssetMenu(fileName = "Science Question", menuName = "Science Question")]
+    public class ScienceQuestion : ScriptableObject
+    {    
+        public string Question;
+        public List<string> Facts;
+
+        public Sprite CorrectAnswer;
+        public List<Sprite> IncorrectAnswers;
+    }
+}
